685349113b0da57196f22d7376826b8040111ceb
Marcelo Lucena <marcelo.lucena@texs.server>
2019-09-28 14:31:33 -0300

    Primeira Aula

---
+++ Modulo01/Exercicios/Desafio.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicios
+{
+    public class Desafio
+    {
+        public void FatorialQuatro()
+        {
+            Console.WriteLine("Fatorial Número 4 é 4x3x2x1="+(4*3*2*1));
+        }
+
+        
+        private int CalcularFatorial(int numero)
+        {
+            int resultado = 1;
+            for (int i = 2; i<=numero; i++)
+            {
+                resultado *= i;
+            }
+            return resultado;
+        }
+
+        public void Fatorial(int numero)
+        {
+            Console.WriteLine($"Fatorial do número {numero} é {CalcularFatorial(numero)}");
+        }
+
+        public void Exercicio01()
+        {
+            Console.WriteLine(">> Exercício 01 << Fatorial de um número estático (4)");
+            Desafio desafio1 = new Desafio();
+            desafio1.FatorialQuatro();
+
+            Console.WriteLine(">> Exercício 01 << Fatorial de um número passado como parametro");
+            desafio1.Fatorial(10);
+
+        }
+
+        public void Exercicio02()
+        {
+            Console.WriteLine(">> Exercício 02 << Fatorial de um número digitado");
+            string str_num_02_1 = Console.ReadLine();
+            int num_02_1 = Convert.ToInt32(str_num_02_1);
+            Fatorial(num_02_1);
+        }
+
+        public void Exercicio03()
+        {
+            Console.WriteLine(">> Exercício 03 << Verificar qual número é maior");
+            Console.Write("Digite o Primeiro Número:");
+            string str_num_03_1 = Console.ReadLine();
+            int num_03_1 = Convert.ToInt32(str_num_03_1);
+            Console.Write("Digite o Segundo Número:");
+            string str_num_03_2 = Console.ReadLine();
+            int num_03_2 = Convert.ToInt32(str_num_03_2);
+            if (num_03_1 == num_03_2)
+            {
+                Console.WriteLine($"O número {num_03_1} é igual ao número {num_03_2}.");
+            }
+            else if (num_03_1 > num_03_2)
+            {
+                Console.WriteLine($"O número {num_03_1} é maior que o número {num_03_2}.");
+            }
+            else
+            {
+                Console.WriteLine($"O número {num_03_2} é maior que o número {num_03_1}.");
+            }
+        }
+
+        public void Exercicio04()
+        {
+            Console.WriteLine(">> Exercício 04 << Verificar se o número é par ou impar");
+            Console.Write("Digite um Número:");
+            string str_num_04_1 = Console.ReadLine();
+            int num_04_1 = Convert.ToInt32(str_num_04_1);
+            if ((num_04_1 % 2) == 0)
+            {
+                Console.WriteLine($"O número {num_04_1} é par.");
+            }
+            else
+            {
+                Console.WriteLine($"O número {num_04_1} é impar.");
+            }
+
+        }
+
+        public void Exercicio05()
+        {
+            Console.WriteLine(">> Exercício 05 << Receber dois números e aplicar a operação");
+            Console.Write("Digite o Primeiro Número:");
+            string str_num_05_1 = Console.ReadLine();
+            int num_05_1 = Convert.ToInt32(str_num_05_1);
+            Console.Write("Digite o Segundo Número:");
+            string str_num_05_2 = Console.ReadLine();
+            int num_05_2 = Convert.ToInt32(str_num_05_2);
+            Console.Write("Digite o Operador:");
+            string operador = Console.ReadLine();
+
+            switch (operador)
+            {
+                case "+":
+                    Console.WriteLine($"O resultadao da soma de {num_05_1} com {num_05_2} é {num_05_1 + num_05_2}.");
+                    break;
+                case "-":
+                    Console.WriteLine($"O resultadao da subtração de {num_05_1} com {num_05_2} é {num_05_1 - num_05_2}.");
+                    break;
+                case "/":
+                    Console.WriteLine($"O resultadao da divisão de {num_05_1} com {num_05_2} é {num_05_1 / num_05_2}.");
+                    break;
+                case "*":
+                    Console.WriteLine($"O resultadao da multiplicação de {num_05_1} com {num_05_2} é {num_05_1 * num_05_2}.");
+                    break;
+                default:
+                    Console.WriteLine("Você não digitou um operador conhecido (+ - / *)");
+                    break;
+            }
+        }
+
+        public void Exercicio06()
+        {
+            Console.WriteLine(">> Exercício 06 << Tabuada de um número digitado");
+            Console.Write("Digite um Número:");
+            string str_num = Console.ReadLine();
+            int num = Convert.ToInt32(str_num);
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{num}*{i}={num*i}");
+            }
+        }
+
+        public void Exercicio07()
+        {
+            Console.WriteLine(">> Exercício 07 << Calcular Em Celsius para Fahrenheit");
+            Console.Write("Digite a Temperatura:");
+            string str_num = Console.ReadLine();
+            int num = Convert.ToInt32(str_num);
+            Console.Write("A Temperatura digitada está em C-Celsius ou F-Fahrenheit:");
+            string str_tipo = Console.ReadLine();
+            switch (str_tipo.ToUpper())
+            {
+                case "C":
+                    Console.WriteLine($" {str_num}Celsius é igual a {CelsiusToFahrenheit(num)} Fahrenheit");
+                    break;
+
+                case "F":
+                    Console.WriteLine($" {str_num}Fahrenheit é igual a {FahrenheitToCelsius(num)} Celsius");
+                    break;
+
+                default:
+                    Console.WriteLine("Você não digitou um valor conhecido (C F).");
+                    break;
+            }
+        }
+
+        private float CelsiusToFahrenheit(int temperatura)
+        {
+            float resultado = 0;
+            resultado = ((temperatura * 9) / 5) +32;
+            return resultado;
+        }
+
+        private float FahrenheitToCelsius(int temperatura)
+        {
+            float resultado = 0;
+            resultado = ((temperatura - 32) * 5) / 9;
+            return resultado;
+        }
+    }
+}
---
+++ Modulo01/Exercicios/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Exercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.Write("Hello World 1!");
+            Console.WriteLine("Hello World 2!");
+
+            string nome;
+            bool ativo;
+            int numero;
+            long numero_longo;
+            decimal valor;
+            DateTime hoje;
+            DateTime dataManual;
+            char tipo;
+
+            int[] array_int = new int[5];
+            string[] array_string = new string[10];
+
+            array_string[0] = "Teste01";
+            array_string[1] = "Teste02";
+            array_string[2] = "Teste03";
+            array_string[3] = "Teste04";
+            array_string[4] = "Teste05";
+            array_string[5] = "Teste06";
+            array_string[6] = "Teste07";
+            array_string[7] = "Teste08";
+            array_string[8] = "Teste09";
+            array_string[9] = "Teste10";
+
+            nome = "Teste";
+            ativo = true;
+            numero = 1;
+            numero_longo = 9999999999;
+            valor = 15_59 + 1_01;
+            hoje = DateTime.Now; //Data Atual
+            dataManual = new DateTime(2019,09,20);
+            tipo = 'S';
+            Console.WriteLine("Digite um valor:");
+            //string variavelUsuario = Console.ReadLine();
+
+            if (ativo)
+            {
+                Console.WriteLine("Situacao......: Ativo");
+            }
+            else
+            {
+                Console.WriteLine("Situacao......: Inativo");
+            }
+
+            Console.WriteLine("Nome..........: " + nome);
+            Console.WriteLine("Numero........: " + numero);
+            Console.WriteLine("Numero Longo..: " + numero_longo);
+            Console.WriteLine("Valor.........: " + valor.ToString("00_00"));
+            Console.WriteLine("Hoje..........: " + hoje.ToString("dd/MM/yyyy"));
+            Console.WriteLine("Tipo.Char.....: " + tipo);
+
+            Console.WriteLine($"Nome..........: {nome}.");
+            Console.WriteLine($"Numero........: {numero}.");
+            Console.WriteLine($"Numero Longo..: {numero_longo}.");
+            Console.WriteLine($"Valor.........: {valor}.");
+            Console.WriteLine($"Hoje..........: {hoje.ToString("dd/MM/yyyy")}.");
+            Console.WriteLine($"Tipo.Char.....: {tipo}");
+
+            //Console.WriteLine($"Valor.Digitado: {variavelUsuario}.");
+
+            Console.Write($"Array.........: (");
+            for (int i = 0; i < array_string.Length; i++)
+            {
+                if (i != 0)
+                {
+                    Console.Write(",");
+                }
+                Console.Write(array_string[i]);
+            }
+            Console.WriteLine(").");
+
+
+            int j = 0;
+            Console.Write($"While.........: (");
+            while (j < 11)
+            {
+                if (j > 0)
+                {
+                    Console.Write(",");
+                }
+                Console.Write(j);
+                ++j;
+            }
+            Console.WriteLine(")");
+            */
+            bool mostrarmenu = true;
+            while (mostrarmenu)
+            {
+                Console.Clear();
+                Console.WriteLine("************ Desafio ***************");
+                Desafio desafio1 = new Desafio();
+                Console.WriteLine(">> Exercício 01 << Fatorial de um número passado como parametro");
+                Console.WriteLine(">> Exercício 02 << Fatorial de um número digitado");
+                Console.WriteLine(">> Exercício 03 << Verificar qual número é maior");
+                Console.WriteLine(">> Exercício 04 << Verificar se o número é par ou impar");
+                Console.WriteLine(">> Exercício 05 << Receber dois números e aplicar a operação");
+                Console.WriteLine(">> Exercício 06 << Tabuada de um número digitado");
+                Console.WriteLine(">> Exercício 07 << Calcular Em Celsius para Fahrenheit");
+                Console.WriteLine(">> Exercício 10 << Sair");
+                Console.Write("Digite a Exercicio:");
+                string exercicio = Console.ReadLine();
+
+                switch (exercicio)
+                {
+                    case "1":
+                    case "01":
+                        desafio1.Exercicio01();
+                        break;
+                    case "2":
+                    case "02":
+                        desafio1.Exercicio02();
+                        break;
+                    case "3":
+                    case "03":
+                        desafio1.Exercicio03();
+                        break;
+                    case "4":
+                    case "04":
+                        desafio1.Exercicio04();
+                        break;
+                    case "5":
+                    case "05":
+                        desafio1.Exercicio05();
+                        break;
+                    case "6":
+                    case "06":
+                        desafio1.Exercicio06();
+                        break;
+
+                    case "7":
+                    case "07":
+                        desafio1.Exercicio07();
+                        break;
+
+                    case "10":
+                        mostrarmenu = false;
+                        break;
+
+                    default:
+                        break;
+                }
+                Console.ReadKey();
+            }
+        }       
+    }
+}
