f75699aabe41d5a973bb25639c7f2ab27c55fded
marcelo.lucena <marcelo.lucena@DD04>
2019-12-09 19:22:04 -0300

    Scaffold

---
+++ Modulo02/MAL.Scaffold/MAL.Scaffold/Data/ScaffoldContext.cs
@@ -0,0 +1,16 @@
+﻿using MAL.Scaffold.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MAL.Scaffold.Data
+{
+    public class ScaffoldContext: DbContext
+    {
+        public ScaffoldContext(DbContextOptions<ScaffoldContext> options) : base(options) { }
+
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
---
+++ Modulo02/MAL.Scaffold/MAL.Scaffold/Models/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MAL.Scaffold.Models
+{
+    public class Produto
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Documento { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
